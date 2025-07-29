6656a3e978e659ddac7d647c1dbf9b030622751b
Jorge Murillo <jorge@eoscostarica.io>
2025-07-29 12:33:05 -0600

    Hero text display fix.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,7 @@ const Hero = () => {
       <div className="container-width">
         <div className="max-w-3xl mx-auto text-center space-y-8">
           <div className="space-y-2 animate-slide-down">
-            <h1 className="mt-4 font-medium leading-tight tracking-tighter px-[16px]">Designer / Technologist
-/ Doer</h1>
+            <h1 className="mt-4 font-medium leading-tight tracking-tighter px-[16px]">Designer / Technologist <br/>/ Doer</h1>
           </div>
           
           <p className="text-lg text-muted-foreground max-w-2xl mx-auto animate-fade-in [animation-delay:200ms]">Helping build the Internet since 1998.</p>
